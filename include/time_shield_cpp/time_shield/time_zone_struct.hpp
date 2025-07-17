0ce33842c57aa07f9b74831e52d0c4f0e42a132e
Yaroslav <elektroyar@yandex.ru>
2025-07-17 21:31:27 +0300

    fix(validation): removed invalid const qualifiers on value return types

--- include/time_shield_cpp/time_shield/time_zone_struct.hpp
+++ include/time_shield_cpp/time_shield/time_zone_struct.hpp
@@ -29,7 +29,7 @@ namespace time_shield {
     /// \param min The minute component of the time.
     /// \param is_positive True if the time zone offset is positive, false if negative.
     /// \return A TimeZoneStruct instance with the provided time components.
-    inline const TimeZoneStruct create_time_zone_struct(
+    inline TimeZoneStruct create_time_zone_struct(
             int hour,
             int min,
             bool is_positive = true) {
@@ -42,7 +42,7 @@ namespace time_shield {
     /// \brief Converts an integer to a TimeZoneStruct.
     /// \param offset The integer to convert.
     /// \return A TimeZoneStruct represented by the given integer.
-    inline const TimeZoneStruct to_time_zone_struct(tz_t offset) {
+    inline TimeZoneStruct to_time_zone_struct(tz_t offset) {
         int abs_val = std::abs(offset);
         int hour = abs_val / SEC_PER_HOUR;
         int min = abs_val % SEC_PER_MIN;
@@ -53,7 +53,7 @@ namespace time_shield {
     /// \ingroup time_structures_time_conversions
     /// \brief Alias for to_time_zone_struct function.
     /// \copydoc to_time_zone_struct
-    inline const TimeZoneStruct to_tz(tz_t offset) {
+    inline TimeZoneStruct to_tz(tz_t offset) {
         return to_time_zone_struct(offset);
     }
 
@@ -63,7 +63,7 @@ namespace time_shield {
     /// \brief Converts a TimeZoneStruct to a string representation.
     /// \param tz The TimeZoneStruct to convert.
     /// \return A string representation of the TimeZoneStruct.
-    inline const std::string time_zone_struct_to_string(const TimeZoneStruct& tz) {
+    inline std::string time_zone_struct_to_string(const TimeZoneStruct& tz) {
         char sign = tz.is_positive ? '+' : '-';
         return std::string(1, sign) + (tz.hour < 10 ? "0" : "") + std::to_string(tz.hour) + ":" + (tz.min < 10 ? "0" : "") + std::to_string(tz.min);
     }
@@ -71,14 +71,14 @@ namespace time_shield {
     /// \ingroup time_structures_time_formatting
     /// \brief Alias for time_zone_struct_to_string function.
     /// \copydoc time_zone_struct_to_string
-    inline const std::string to_string(const TimeZoneStruct& tz) {
+    inline std::string to_string(const TimeZoneStruct& tz) {
         return time_zone_struct_to_string(tz);
     }
 
     /// \ingroup time_structures_time_formatting
     /// \brief Alias for time_zone_struct_to_string function.
     /// \copydoc time_zone_struct_to_string
-    inline const std::string to_str(const TimeZoneStruct& tz) {
+    inline std::string to_str(const TimeZoneStruct& tz) {
         return time_zone_struct_to_string(tz);
     }
 
@@ -88,21 +88,21 @@ namespace time_shield {
     /// \brief Converts a TimeZoneStruct to a single integer representation.
     /// \param tz The TimeZoneStruct to convert.
     /// \return An integer representing the TimeZoneStruct.
-    inline const tz_t time_zone_struct_to_offset(const TimeZoneStruct& tz) {
+    inline tz_t time_zone_struct_to_offset(const TimeZoneStruct& tz) {
         return (tz.is_positive ? 1 : -1) * (tz.hour * SEC_PER_HOUR + tz.min * SEC_PER_MIN);
     }
 
     /// \ingroup time_structures_time_conversions
     /// \brief Alias for time_zone_struct_to_offset function.
     /// \copydoc time_zone_struct_to_offset
-    inline const tz_t tz_to_offset(const TimeZoneStruct& tz) {
+    inline tz_t tz_to_offset(const TimeZoneStruct& tz) {
         return time_zone_struct_to_offset(tz);
     }
 
     /// \ingroup time_structures_time_conversions
     /// \brief Alias for time_zone_struct_to_offset function.
     /// \copydoc time_zone_struct_to_offset
-    inline const tz_t to_offset(const TimeZoneStruct& tz) {
+    inline tz_t to_offset(const TimeZoneStruct& tz) {
         return time_zone_struct_to_offset(tz);
     }
 
--- include/time_shield_cpp/time_shield/validation.hpp
+++ include/time_shield_cpp/time_shield/validation.hpp
@@ -51,21 +51,21 @@ namespace time_shield {
     /// \param year Year to check.
     /// \return true if the year is a leap year, false otherwise.
     template<class T = year_t>
-    constexpr const bool is_leap_year_date(T year) noexcept {
+    constexpr bool is_leap_year_date(T year) noexcept {
         return ((year & 3) == 0 && ((year % 25) != 0 || (year & 15) == 0));
     }
 
     /// \brief Alias for is_leap_year_date function.
     /// \copydoc is_leap_year_date
     template<class T = year_t>
-    constexpr const bool check_leap_year(T year) noexcept {
+    constexpr bool check_leap_year(T year) noexcept {
         return is_leap_year_date(year);
     }
 
     /// \brief Alias for is_leap_year_date function.
     /// \copydoc is_leap_year_date
     template<class T = year_t>
-    constexpr const bool leap_year(T year) noexcept {
+    constexpr bool leap_year(T year) noexcept {
         return is_leap_year_date(year);
     }
 

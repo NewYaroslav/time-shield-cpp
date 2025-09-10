f252ea4e76c0bd45332ea382aca87a4d384ca441
Aster Seker <git.electroyar@gmail.com>
2025-09-10 06:58:46 +0300

    fix: update vcpkg port hash

--- vcpkg-overlay/ports/time-shield-cpp/portfile.cmake
+++ vcpkg-overlay/ports/time-shield-cpp/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO NewYaroslav/time-shield-cpp
     REF v1.0.2
-    SHA512 94e226fbef842599fdb2c88e0a3c38baddf8f0c02606e5533b2f7e6cb518c3592c66ef29088d34b2444712276af13f0492c326c551e5c0712b79a2a86cddbca8
+    SHA512 cbd0a981af568ca41d9350911f71535b8c7ed4466b36bbc9ceff9c33e0d9f2ed22ca86950836836ca7c2997f316b7612efa9234225bc41f2f0e5e9c5119358b4
 )
 
 vcpkg_cmake_configure(

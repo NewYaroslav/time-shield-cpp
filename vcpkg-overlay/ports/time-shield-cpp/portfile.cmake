51ed117895f2a4d34416599497fdfffe1dec30d3
Aster Seker <git.electroyar@gmail.com>
2025-09-10 07:36:24 +0300

    Use local source for overlay port

--- vcpkg-overlay/ports/time-shield-cpp/portfile.cmake
+++ vcpkg-overlay/ports/time-shield-cpp/portfile.cmake
@@ -1,9 +1,4 @@
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO NewYaroslav/time-shield-cpp
-    REF v1.0.2
-    SHA512 cbd0a981af568ca41d9350911f71535b8c7ed4466b36bbc9ceff9c33e0d9f2ed22ca86950836836ca7c2997f316b7612efa9234225bc41f2f0e5e9c5119358b4
-)
+set(SOURCE_PATH "${CURRENT_PORT_DIR}/../../..")
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}

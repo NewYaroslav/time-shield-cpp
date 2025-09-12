5658145066990acd37b12fc73df77fdb055ddbb5
Aster Seker <git.electroyar@gmail.com>
2025-09-12 03:57:25 +0300

    chore(license): add SPDX identifiers

--- include/time_shield.hpp
+++ include/time_shield.hpp
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 #pragma once
 #ifndef _TIME_SHIELD_HPP_INCLUDED
 #define _TIME_SHIELD_HPP_INCLUDED
--- include/time_shield/time_utils.hpp
+++ include/time_shield/time_utils.hpp
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 #pragma once
 #ifndef _TIME_SHIELD_TIME_UTILS_HPP_INCLUDED
 #define _TIME_SHIELD_TIME_UTILS_HPP_INCLUDED

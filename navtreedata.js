67dc8a8ecc80b2b77eca914c2bf5f02bca25e79b
NewYaroslav <NewYaroslav@users.noreply.github.com>
2026-07-02 18:05:20 +0000

    deploy: d5bb4aac0d7e9193173d3df96b3e7a0e066bfbbf

--- navtreedata.js
+++ navtreedata.js
@@ -189,6 +189,6 @@ var NAVTREEINDEX =
 "time__formatting__example_8cpp_source.html"
 ];
 
-var SYNCONMSG = 'click to disable panel synchronization';
-var SYNCOFFMSG = 'click to enable panel synchronization';
-var LISTOFALLMEMBERS = 'List of all members';
+const SYNCONMSG = 'click to disable panel synchronization';
+const SYNCOFFMSG = 'click to enable panel synchronization';
+const LISTOFALLMEMBERS = 'List of all members';
